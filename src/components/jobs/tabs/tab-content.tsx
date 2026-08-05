fdf1b64074f2ba4cbc8e05c7b017cdebd34d1745
iam-awaneesh <aw64an@gmail.com>
2026-08-05 20:02:38 +0530

    Update the propers

--- src/components/jobs/tabs/tab-content.tsx
+++ src/components/jobs/tabs/tab-content.tsx
@@ -1,16 +1,18 @@
 "use client"
 
 import { TabsContent } from "@/components/ui/tabs"
+import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
 interface JobTabContentProps {
   value: string
   children: ReactNode
+  className?: string;
 }
 
-export function JobTabContent({ value, children }: JobTabContentProps) {
+export function JobTabContent({ value, children,className, }: JobTabContentProps) {
   return (
-    <TabsContent value={value} className="border-none p-0 ">
+    <TabsContent value={value} className={cn("border-none p-0", className)}>
       {children}
     </TabsContent>
   )
